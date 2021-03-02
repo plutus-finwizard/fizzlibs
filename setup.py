1881871667b836752109700732d83c523cc30ddf
Satish Rabha <satish@fisdom.com>
2021-03-02 16:11:13 +0530

    1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='fizzlibs',
-    version='1.0.4',
+    version='1.0.5',
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
