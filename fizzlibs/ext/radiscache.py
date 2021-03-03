ea36f34a5456c84f15bd2dabf53cd0c1710c8065
Satish Rabha <satish@fisdom.com>
2021-03-03 16:30:00 +0530

    1.0.9 redis

--- fizzlibs/ext/radiscache.py
+++ fizzlibs/ext/radiscache.py
@@ -1,8 +1,11 @@
 import os
+import json
 import redis
+
 
 class RADIS_AUTHENTICATION_ERROR(Exception):
     pass
+
 
 def __get_radis__():
     if not all([os.environ.get('REDIS_SERVICE_HOST'), os.environ.get('REDIS_SERVICE_PORT')]):
@@ -15,7 +18,19 @@ def __get_radis__():
         )
 
 
+def __sanitize_key__(key):
+    suffix = os.environ.get('GKE_SOFTWARE_ID')
+
+    if suffix:
+        return f'{suffix}-key'
+
+    return key
+
+
 def set(key, value, **kwargs):
+    key = __sanitize_key__(key)
+    value = json.dumps(value) if value else value
+
     r = __get_radis__()
     r.set(key, value)
 
@@ -24,10 +39,17 @@ def set(key, value, **kwargs):
 
 
 def get(key):
-    return __get_radis__().get(key)
+    key = __sanitize_key__(key)
+    value = __get_radis__().get(key)
+
+    if value:
+        return json.loads(value)
+
+    return value
 
 
 def delete(key):
+    key = __sanitize_key__(key)
     return __get_radis__().delete(key)
 
 
@@ -35,12 +57,12 @@ def delete(key):
 if __name__ == "__main__":
     import time
 
-    set('name', 'buzz')
-    print (f"{b'buzz'} == {get('name')}")
-    print (f"{b'buzz'} == {get('name')}")
+    set('c', 'buzz')
+    print (f"{b'buzz'} == {get('c')}")
+    print (f"{b'buzz'} == {get('c')}")
 
-    delete('name')
-    print (f"{'None'} == {get('name')}")
+    delete('c')
+    print (f"{'None'} == {get('c')}")
 
     set('amy', 'peterson', time=0)
     print (f"{b'perterson'} == {get('amy')}")
@@ -54,3 +76,6 @@ if __name__ == "__main__":
     print ('Sleep (5)')
     time.sleep(5)
     print (f"{'None'} == {get('pheobe')}")
+
+    set('k', {'x': 'hello'})
+    set('k', 1)
